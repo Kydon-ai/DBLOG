ccf5d215e319b154c6e30d96a32716a0d5353ea4
Kydon <130697271+kydon-ai@users.noreply.github.com>
2026-01-17 17:22:49 +0800

    feat:移植青训营部分设计进入主版本

--- src/routes/topMenu.tsx
+++ src/routes/topMenu.tsx
@@ -1,76 +1,72 @@
 // TopMenu.tsx
 import React, { useState } from 'react';
-import { Button, Dropdown, Menu, Row,Avatar, Space } from 'antd';
+import { Button, Dropdown, Menu, Row, Avatar, Space } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useWindowSize } from '../utils/windowContext/win';
 import { DownOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 const TopMenu: React.FC = () => {
     const [current, setCurrent] = useState('home');
-    const { size,isHorizontal } = useWindowSize();
+    const { size, isHorizontal } = useWindowSize();
 
-    console.log("查看全屏宽高：",size.width,size.height,isHorizontal)
+    console.log("查看全屏宽高：", size.width, size.height, isHorizontal)
     const items = [
         {
-            key: 'find-shit',
-            label: '史海淘金',
+            key: 'home',
+            label: '主页',
         },
         {
             key: 'about-project',
             label: '关于项目',
         },
         {
-            key: 'front-source',
-            label: '前端资源',
-        },
-        {
-            key: 'friend-chain',
-            label: '网站友链',
+            key: 'document-center',
+            label: '文档中心',
         },
         {
             key: 'concat-me',
             label: '作者详情',
         },
         {
-            key: 'home',
-            label: '主页',
+            key: 'friend-chain',
+            label: '网站友链',
         },
     ];
     const route = useNavigate()
     const onClick = (e: { key: string }) => {
         setCurrent(e.key);
-        console.log('打印key',e.key)
+        console.log('打印key', e.key)
         route("/" + e.key)
     };
     const menus = (
         <Menu
-          selectedKeys={[current]}
-          mode="vertical"
-          onClick={onClick}
-          items={items}
+            selectedKeys={[current]}
+            mode="vertical"
+            onClick={onClick}
+            items={items}
         />
-      );
+    );
     const handleClickLogo = () => {
         // console.log(window.location.href ,window.location.href)
-        if (window.location.href != window.location.origin + '/')  window.location.href = window.location.origin
+        if (window.location.href != window.location.origin + '/') window.location.href = window.location.origin
     }
     return (
         <Row>
-            <div style={{ display: 'flex',justifyContent: 'space-between',width: '100%'}}>
-                <div style={{ display: 'flex', alignItems: 'center',cursor:"pointer"}} onClick={handleClickLogo}>
-                    <img style={{height: '2rem', width: '2rem',marginRight:'5px'}} src="/img/yctf.jpg" alt="LOGO加载失败了" />
-                        
-                    <div style={{ zoom: "60%"}}>
-                        <p style={{fontSize:'2rem',textAlign: 'justify',margin: 0,fontWeight: 'lighter'}}>DBLOG<span style={{display: 'inline-block',width: '0%'}}></span></p>
-                        <p style={{fontSize:'1rem',textAlign: 'justify',margin: 0,fontWeight: 'bolder'}}>雏草姬的温暖港湾<span style={{display: 'inline-block',width: '0%'}}></span></p>
+            <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                <div style={{ display: 'flex', alignItems: 'center', cursor: "pointer" }} onClick={handleClickLogo}>
+                    <img style={{ height: '2rem', width: '2rem', marginRight: '5px' }} src="/img/yctf.jpg" alt="LOGO加载失败了" />
+
+                    <div style={{ zoom: "60%" }}>
+                        <p style={{ fontSize: '2rem', textAlign: 'justify', margin: 0, fontWeight: 'lighter' }}>DBLOG<span style={{ display: 'inline-block', width: '0%' }}></span></p>
+                        <p style={{ fontSize: '1rem', textAlign: 'justify', margin: 0, fontWeight: 'bolder' }}>雏草姬的温暖港湾<span style={{ display: 'inline-block', width: '0%' }}></span></p>
                     </div>
                 </div>
                 {isHorizontal ? (
-                    <div style={{ display:'flex'}}>
+                    <div style={{ display: 'flex' }}>
                         <Menu
-                        onClick={onClick}
-                        selectedKeys={[current]}
-                        mode="horizontal"
-                        items={items}
+                            onClick={onClick}
+                            selectedKeys={[current]}
+                            mode="horizontal"
+                            items={items}
                         />
                         <Avatar src={"/img/ht.gif"} size='large'></Avatar>
                     </div>
